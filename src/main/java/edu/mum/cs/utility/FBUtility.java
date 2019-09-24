a736569fa7fa049a818ff59b886056334e8b0f7f
lusamdebonfils <samuelluswata@gmail.com>
2019-09-24 17:22:31 -0500

    Dao's fully implemented!!!!

--- src/main/java/edu/mum/cs/utility/FBUtility.java
+++ src/main/java/edu/mum/cs/utility/FBUtility.java
@@ -7,9 +7,9 @@ import javax.servlet.ServletContext;
 
 public class FBUtility {
     public static EntityManager getEntityManager(ServletContext servletContext) {
-        //EntityManagerFactory emf = (EntityManagerFactory) servletContext.getAttribute("emFactory");
-        //EntityManager em = emf.createEntityManager();
-        return null;
+        EntityManagerFactory emf = (EntityManagerFactory) servletContext.getAttribute("emFactory");
+        EntityManager em = emf.createEntityManager();
+        return em;
     }
     public static String getImgPath(ServletContext servletContext) {
         return servletContext.getInitParameter("imgPath");
